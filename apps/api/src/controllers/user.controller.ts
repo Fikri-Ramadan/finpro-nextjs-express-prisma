f335f62652373c024dd83eb2c8c985371f4726dd
Fikri Ramadan <fikrinr.tech@gmail.com>
2024-01-19 17:52:27 +0700

    feat(api): getUser, getPoints

---
+++ apps/api/src/controllers/user.controller.ts
@@ -0,0 +1,50 @@
+import prisma from "@/prisma";
+import { User } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
+
+interface IGetUserInfoRequest extends Request {
+  user?: User,
+}
+
+export class UserController {
+  async getPoints(req: IGetUserInfoRequest, res: Response, next: NextFunction) {
+    try {
+      const totalUsedReferral = await prisma.referralUsage.findMany({
+        where: {
+          referralCode: {
+            userId: req.user?.id
+          },
+          AND: {
+            expiryDate: {
+              gt: new Date()
+            },
+            usageDate: null,
+          }
+        }
+      });
+
+      const points = totalUsedReferral.length * 10000;
+
+      return res.status(200).json({
+        success: true,
+        points
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getUsers(req: IGetUserInfoRequest, res: Response, next: NextFunction) {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: req.user?.id
+        }
+      });
+
+      res.status(200).json({ user });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
---
+++ apps/api/src/routers/user.router.ts
@@ -0,0 +1,29 @@
+import { UserController } from "@/controllers/user.controller";
+import { AuthMiddleware } from "@/middlewares/auth.middleware";
+import { Router } from "express";
+
+export class UserRouter {
+  private router: Router;
+  private userController: UserController;
+  private authMiddleware: AuthMiddleware;
+
+  constructor() {
+    this.router = Router();
+    this.userController = new UserController();
+    this.authMiddleware = new AuthMiddleware();
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get('/points',
+      this.authMiddleware.verifyToken,
+      this.userController.getPoints);
+    this.router.get('/', 
+    this.authMiddleware.verifyToken,
+    this.userController.getUsers)
+  }
+
+  getRouter() {
+    return this.router;
+  }
+}
