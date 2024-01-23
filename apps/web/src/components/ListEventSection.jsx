d03116ebc2b37416ce6d63c861010b7f3d52506e
IbnuAfif <ibnuafif015@gmail.com>
2024-01-23 15:25:33 +0700

    fix: get event from database

--- apps/web/src/components/ListEventSection.jsx
+++ apps/web/src/components/ListEventSection.jsx
@@ -1,92 +1,14 @@
 import Image from 'next/image';
+import axios from 'axios';
+import Link from 'next/link';
+// import { useRouter } from 'next/router';
+import { redirect } from 'next/navigation';
 
-export default function ListEventSection() {
-  const eventLists = [
-    {
-      id: 1,
-      name: 'Event 1',
-      location: 'Jakarta',
-      title: 'Lorem Ipsum Dolor',
-      image: '/box.png',
-    },
-    {
-      id: 2,
-      name: 'Event 2',
-      location: 'Jakarta',
-      title: 'Lorem Ipsum Dolor',
-      image: '/box.png',
-    },
-    {
-      id: 3,
-      name: 'Event 3',
-      location: 'Jakarta',
-      title: 'Lorem Ipsum Dolor',
-      image: '/box.png',
-    },
-    {
-      id: 4,
-      name: 'Event 4',
-      location: 'Jakarta',
-      title: 'Lorem Ipsum Dolor',
-      image: '/box.png',
-    },
-    {
-      id: 5,
-      name: 'Event 5',
-      location: 'Jakarta',
-      title: 'Lorem Ipsum Dolor',
-      image: '/box.png',
-    },
-    {
-      id: 6,
-      name: 'Event 6',
-      location: 'Jakarta',
-      title: 'Lorem Ipsum Dolor',
-      image: '/box.png',
-    },
-    {
-      id: 7,
-      name: 'Event 7',
-      location: 'Jakarta',
-      title: 'Lorem Ipsum Dolor',
-      image: '/box.png',
-    },
-    {
-      id: 8,
-      name: 'Event 8',
-      location: 'Jakarta',
-      title: 'Lorem Ipsum Dolor',
-      image: '/box.png',
-    },
-    {
-      id: 9,
-      name: 'Event 9',
-      location: 'Jakarta',
-      title: 'Lorem Ipsum Dolor',
-      image: '/box.png',
-    },
-    {
-      id: 10,
-      name: 'Event 10',
-      location: 'Jakarta',
-      title: 'Lorem Ipsum Dolor',
-      image: '/box.png',
-    },
-    {
-      id: 11,
-      name: 'Event 11',
-      location: 'Jakarta',
-      title: 'Lorem Ipsum Dolor',
-      image: '/box.png',
-    },
-    {
-      id: 12,
-      name: 'Event 12',
-      location: 'Jakarta',
-      title: 'Lorem Ipsum Dolor',
-      image: '/box.png',
-    },
-  ];
+export default async function ListEventSection() {
+  const res = await axios.get('http://localhost:8000/api/events');
+  const events = await res?.data?.results;
+  console.log(events);
+  // const router = useRouter();
   return (
     <section className="md:px-36 md:py-8">
       <div className="px-6 mx-auto flex flex-row justify-between items-center">
@@ -95,25 +17,27 @@ export default function ListEventSection() {
       </div>
       <div className="p-6 grid grid-cols-2 gap-x-6 gap-y-8 md:grid-cols-2 lg:grid-cols-3">
         {/* Start Product */}
-        {eventLists.map((event, i) => (
-          <article key={i} className="">
-            <div className="flex flex-col shadow-lg bg-gray-50 p-10 items-center border rounded-xl">
-              <a href="#">
-                <Image src={event.image} alt="" width={100} height={100} />
-              </a>
-            </div>
-            <div className="flex flex-row gap-3  justify-start  p-1 items-center">
-              <h3 className="  py-1 text-sm leadi uppercase w-auto">
-                {event.name},
-              </h3>
-              <p className="font-bold text-sm italic">{event.location}</p>
-            </div>
-            <div className="flex flex-col flex-1 px-1 py-0">
-              <h3 className="flex-1 py-0 text-base font-semibold leadi uppercase">
-                {event.title}
-              </h3>
-            </div>
-          </article>
+        {events.map((event, i) => (
+          <Link href={`/event/${event.id}`}>
+            <article key={i} className="">
+              <div className="flex flex-col shadow-lg bg-gray-50 p-10 items-center border rounded-xl">
+                <a href="#">
+                  <Image src={'/band.png'} alt="" width={100} height={100} />
+                </a>
+              </div>
+              <div className="flex flex-row gap-3  justify-start  p-1 items-center">
+                <h3 className="  py-1 text-sm leadi uppercase w-auto">
+                  {event.name},
+                </h3>
+                <p className="font-bold text-sm italic">{event.location}</p>
+              </div>
+              <div className="flex flex-col flex-1 px-1 py-0">
+                <h3 className="flex-1 py-0 text-base font-semibold leadi uppercase">
+                  {event.title}
+                </h3>
+              </div>
+            </article>
+          </Link>
         ))}
 
         {/* End Product */}
