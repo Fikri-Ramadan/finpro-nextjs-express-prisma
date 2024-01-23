40873aea8179cbb85a3bdc3df1e9962faef46228
Fikri Ramadan <fikrinr.tech@gmail.com>
2024-01-23 17:10:41 +0700

    chore: remove unnecessary code

--- apps/web/src/components/auth/Login.jsx
+++ apps/web/src/components/auth/Login.jsx
@@ -1,7 +1,5 @@
 import Image from 'next/image';
-import { Input } from '../ui/input';
 import Link from 'next/link';
-import { Button } from '../ui/button';
 import FormLogin from './FormLogin';
 
 export default function Login() {
@@ -26,30 +24,7 @@ export default function Login() {
       </div>
       <div className="w-full lg:w-1/3 h-full flex flex-col justify-center">
         <p className="lg:hidden text-2xl font-bold mb-8">Login</p>
-          <FormLogin />
-        {/* <p className="lg:hidden text-2xl font-bold mb-8">Login</p>
-        <div className="w-full flex flex-col gap-4">
-          <Input
-            type="email"
-            placeholder="Enter Email"
-            className="bg-blue-50 border-slate-400"
-          />
-          <Input
-            type="password"
-            placeholder="Enter Password"
-            className="bg-blue-50 border-slate-400"
-          />
-        </div>
-        <Link href="" className="text-right mt-2 text-slate-700 text-sm">
-          Recover Password ?
-        </Link>
-        <Button className="mt-8">Sign In</Button>
-        <p className="text-sm font-medium mt-4 lg:hidden">
-          if you don&apos;t have an account you can{' '}
-          <Link href="/register" className="text-blue-700 font-semibold">
-            register here !
-          </Link>
-        </p> */}
+        <FormLogin />
       </div>
     </div>
   );
