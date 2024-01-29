e5f96edf638acd5c468767c8e9de3a80b00ef5a8
Muhammad Ibnu Afif <88562045+IbnuAfif@users.noreply.github.com>
2024-01-29 12:23:37 +0700

    fix: search bar

--- apps/web/src/app/event/search/page.js
+++ apps/web/src/app/event/search/page.js
@@ -1,38 +1,18 @@
-'use client';
-
 import HeroSection from '@/components/HeroSection';
 import ListEventSection from '@/components/ListEventSection';
 import SearchBarEvent from '@/components/SearchBarEvent';
 import customAxios from '@/lib/axios';
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
 
-export default function searchEvent({}) {
-  const searchParams = useSearchParams();
-  const name = searchParams.get('name');
-  // const response = await customAxios.get(`/api/events?name=${name}`);
-  // const searchEvents = await response?.data?.results;
-  const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetching = async () => {
-      const response = await customAxios.get(`/api/events?name=${name}`);
-      const searchEvents = await response?.data?.results;
-      setData(searchEvents);
-      setLoading(false);
-    };
-    fetching();
-  }, []);
-
-  if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No profile data</p>;
-
+export default async function searchEvent({ searchParams }) {
+  const response = await customAxios.get(
+    `/api/events?name=${searchParams.name}`,
+  );
+  const searchEvents = await response?.data?.results;
   return (
     <section>
       <HeroSection />
       <SearchBarEvent />
-      <ListEventSection apiEvents={data} />
+      <ListEventSection apiEvents={searchEvents} />
     </section>
   );
 }
