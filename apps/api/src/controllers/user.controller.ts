634283c1ce33b047482e85bf289d2b4f05a5be07
Fikri Ramadan <fikrinr.tech@gmail.com>
2024-01-30 10:31:29 +0700

    feat(frontend): transactions table

--- apps/api/src/controllers/user.controller.ts
+++ apps/api/src/controllers/user.controller.ts
@@ -82,7 +82,7 @@ export class UserController {
           event: true
         },
         orderBy: {
-          date: 'asc'
+          date: 'desc'
         }
       });
 
--- apps/web/src/components/auth/TransactionDialog.jsx
+++ apps/web/src/components/auth/TransactionDialog.jsx
@@ -20,21 +20,24 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { Button } from '../ui/button';
 import useTransactions from '@/hooks/useTransactions';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function TransactionDialog() {
+  const [isOpen, setOpen] = useState(false);
   const { data, isLoading } = useTransactions();
+  const router = useRouter();
 
   return (
-    <Dialog className="w-[1000px]">
-      <DialogTrigger asChild>
+    <Dialog open={isOpen}>
+      <DialogTrigger asChild onClick={() => setOpen(true)}>
         <div className="pl-[8px] py-[4px] flex items-center cursor-pointer">
           <ArrowLeftRight className="mr-2 h-4 w-4" />
           <span className="text-sm">History Transactions</span>
         </div>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-2xl">
+      <DialogContent className="sm:max-w-2xl max-h-[500px] overflow-y-scroll flex flex-col justify-center items-center">
         <DialogHeader>
           <DialogTitle>Your Transaction History</DialogTitle>
           <DialogDescription>
@@ -42,49 +45,60 @@ export default function TransactionDialog() {
           </DialogDescription>
         </DialogHeader>
 
-        <Table>
-          <TableCaption>A list of your recent transactions.</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="">Invoice</TableHead>
-              <TableHead>Amount Paid</TableHead>
-              <TableHead>Point Used</TableHead>
-              <TableHead>Discount Applied</TableHead>
-              <TableHead>Date</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {isLoading ? (
-              <div>Loading...</div>
-            ) : (
-              data.map((transaction, i) => {
-                return (
-                  <TableRow key={i}>
-                    <TableCell className="hover:cursor-pointer hover:underline">
-                      {transaction.event.name}
-                    </TableCell>
-                    <TableCell>
-                      Rp. {transaction.amountPaid || 'Free'}
-                    </TableCell>
-                    <TableCell>{transaction.pointUsed || 0}</TableCell>
-                    <TableCell>{transaction.discountApplied || 0}</TableCell>
-                    <TableCell>
-                      {new Date(transaction.date).toLocaleString('en-US')}
-                    </TableCell>
-                  </TableRow>
-                );
-              })
-            )}
-          </TableBody>
-        </Table>
+        <div className="max-h-[300px] w-[300px] sm:w-full">
+          <Table>
+            {/* <TableCaption>A list of your recent transactions.</TableCaption> */}
+            <TableHeader>
+              <TableRow>
+                <TableHead>No.</TableHead>
+                <TableHead>Event Name</TableHead>
+                <TableHead>Amount Paid</TableHead>
+                <TableHead>Point Used</TableHead>
+                <TableHead>Discount Applied</TableHead>
+                <TableHead>Date</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody className="overflow-scroll">
+              {isLoading ? (
+                <div>Loading...</div>
+              ) : (
+                data.map((transaction, i) => {
+                  return (
+                    <TableRow key={i}>
+                      <TableCell>{i + 1}</TableCell>
+                      <TableCell
+                        className="hover:cursor-pointer hover:underline"
+                        onClick={() => {
+                          router.push(`/event/${transaction.event.id}`);
+                          router.refresh('');
+                          setOpen(false);
+                        }}
+                      >
+                        {transaction.event.name}
+                      </TableCell>
+                      <TableCell>
+                        Rp. {transaction.amountPaid || 'Free'}
+                      </TableCell>
+                      <TableCell>{transaction.pointUsed || 0}</TableCell>
+                      <TableCell>{transaction.discountApplied || 0}</TableCell>
+                      <TableCell>
+                        {new Date(transaction.date).toLocaleString('en-US')}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })
+              )}
+            </TableBody>
+          </Table>
+        </div>
 
-        <DialogFooter className="sm:justify-start">
+        {/* <DialogFooter className="sm:justify-start">
           <DialogClose asChild>
             <Button type="button" variant="secondary">
               Close!
             </Button>
           </DialogClose>
-        </DialogFooter>
+        </DialogFooter> */}
       </DialogContent>
     </Dialog>
   );
